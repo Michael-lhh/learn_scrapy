606d553f22435121de8fe8b9affaaf67799e6c4c
Michaellok <luohoiho55@gmail.com>
2022-04-21 01:05:09 +0800

    Add selector

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
-# scrapy
+# Scrapy
+
 Just Learn and Push
+
+## CSS selector
+
+```html
+<h3><a href="catalogue/tipping-the-velvet_999/index.html" title="Tipping the Velvet">Tipping the Velvet</a></h3>
+```
+
+extract title value
+
+response.css('article.product_pod').css('h3 > a::attr(title) ').extract_first()
+
+extract_first(): extract the first node or first text error may happen if deserve msg at second position
+
+
+
+
--- example/example/spiders/books.py
+++ example/example/spiders/books.py
@@ -9,13 +9,17 @@ class BookSpider(scrapy.Spider):
   def parse(self, response):
 
     for book in response.css('article.product_pod'):
-      name = book.xpath('./h3/a/@title').extract_first()
-
-      price = book.css('p.price_color::text').extract_first()
-      yield {
-        'name': name,
-        'price': price,
-      }
+        # name = book.xpath('./h3/a/@title').extract_first()
+        name = book.css('h3>a::attr(title) ').extract_first()
+        price = book.css('p.price_color::text').extract_first()
+        status = book.css('p.instock.availability::text').extract()[1].replace(' ', '').replace('\n', ''),
+        stars = book.css('article.product_pod p::attr(class)').extract_first().partition(' ')[2],
+        yield {
+            'name': name,
+            'price': price,
+            'status': status,
+            'stars': stars,
+        }
 
     next_url = response.css('ul.pager li.next a::attr(href)').extract_first()
 
