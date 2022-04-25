b330e3acb94303d379c71ae4ebe96afea72bbc93
Michaellok <luohoiho55@gmail.com>
2022-04-25 22:18:04 +0800

    raise duplicate item warming

--- example/example/pipelines.py
+++ example/example/pipelines.py
@@ -6,8 +6,38 @@
 
 # useful for handling different item types with a single interface
 from itemadapter import ItemAdapter
+from scrapy.exceptions import DropItem
 
 
 class ExamplePipeline:
     def process_item(self, item, spider):
+        return item
+
+
+class PriceConverterPipeline(object):
+
+    exchange_rate = 8.5309
+
+    def process_item(self, item, spider):
+
+        price = float(item['price'][1:]) * self.exchange_rate
+
+        item['price'] = 'RMB%.2f' % price
+
+        return item
+
+
+class DuplicatesPipeline(object):
+
+    def __init__(self):
+        self.book_set = set()
+
+
+    def process_item(self, item, spider):
+        name = item['name']
+
+        if name in self.book_set:
+            raise DropItem("Duplicate book found: %s" % item)
+
+        self.book_set.add(name)
         return item
--- example/example/settings.py
+++ example/example/settings.py
@@ -62,9 +62,10 @@ ROBOTSTXT_OBEY = True
 
 # Configure item pipelines
 # See https://docs.scrapy.org/en/latest/topics/item-pipeline.html
-#ITEM_PIPELINES = {
-#    'example.pipelines.ExamplePipeline': 300,
-#}
+ITEM_PIPELINES = {
+    'example.pipelines.PriceConverterPipeline': 300,
+    'example.pipelines.DuplicatesPipeline': 350,
+}
 
 # Enable and configure the AutoThrottle extension (disabled by default)
 # See https://docs.scrapy.org/en/latest/topics/autothrottle.html
